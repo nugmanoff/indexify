a1c7e24d45beddc5d46982b0bfc571b567c9a347
AidarNugmanoff <a.nugmanoff@gmail.com>
2017-12-31 00:31:52 +0600

    feat(Package): add Alamofire dependency

--- Package.swift
+++ Package.swift
@@ -6,14 +6,16 @@ import PackageDescription
 let package = Package(
     name: "indexify",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "indexify",
-            dependencies: []),
+            dependencies: ["indexify_core"]
+        ),
+        .target(
+            name: "indexify_core",
+            dependencies: ["Alamofire"]
+        )
     ]
 )
--- Sources/indexify/main.swift
+++ Sources/indexify/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import indexify_core
+
+let indexify = Indexify()
+
+do {
+    try indexify.run()
+} catch {
+    print("Whoops! An error occurred: \(error)")
+}
